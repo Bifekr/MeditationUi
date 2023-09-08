283ab8f03f298a8d966db9494249c86478f18593
miladj796 <miladjamshidpur@gmail.com>
2023-09-08 19:23:57 +0330

    Initial commit

---
+++ app/src/main/java/ir/minicartoon/meditatonui/MainActivity.kt
@@ -0,0 +1,26 @@
+package ir.minicartoon.meditatonui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ir.minicartoon.meditatonui.ui.theme.MeditatonUiTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MeditatonUiTheme {
+
+                
+            }
+        }
+    }
+}
+
